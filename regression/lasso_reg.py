5bb434930c4050b8e14e8887efaf84c0cc3256f0
NewLaptop <piszczekwiktor@gmail.com>
2025-10-23 16:34:36 +0200

    test train loop

--- regression/lasso_reg.py
+++ regression/lasso_reg.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from sim_network import SimNetwork
 from bi_network import BiNetwork
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import SGDRegressor
 
 
 class LassoReg:
@@ -22,9 +22,15 @@ class LassoReg:
         self.sim_network = SimNetwork(small_matrix)
         self.user_features = user_features
         self.ro = ro
-        self.reg_model = Lasso(alpha=0.1)
+        self.reg_model = SGDRegressor(
+            penalty='l1',
+            alpha=0.001,
+            max_iter=1000,
+            tol=1e-3,
+            warm_start=True
+        )
 
-        self.connections_df = None
+        self.possible = None
         self.video_features = None
 
         self.t = 0
@@ -48,13 +54,15 @@ class LassoReg:
         # 0 is the pop mean for standardized features
         videos = possible['video_id'].unique()
         self.video_features = pd.DataFrame(0.0, index=videos, columns=user_feature_columns)
-        self.connections_df = possible
+        self.possible = possible
 
-    def step(self):
+        self.train()
+
+    def step(self, connections_at_t):
         # update video features based on connections at t
         # using exponentially weighted moving average with bias correction
-        connections_at_t = self.bi_network.connections_at_t(self.t)
         if connections_at_t.empty:
+            self.t += 1
             return
         # the values to update with is the mean of user features of users who watched the video at t
         merged = connections_at_t.reset_index(drop=True).merge(self.user_features, on='user_id', how='left')
@@ -69,9 +77,92 @@ class LassoReg:
             (changing_videos * self.ro + video_user_features * (1 - self.ro))/(1 - self.ro ** (self.t + 1))
 
         self.t += 1
+        # set possible connections
+        self.possible = self.bi_network.possible_at_t(self.t)
 
-        # set possible concetions
-        self.connections_df = self.bi_network.possible_at_t(self.t)
+    def train_step(self, train_connections):
+
+        if train_connections.empty:
+            return
+
+        train_connections = train_connections.merge(
+            self.user_features,
+            on='user_id',
+            how='left',
+            suffixes=('', '_user')
+        )
+
+        train_connections = train_connections.merge(
+            self.video_features.reset_index(drop=False, names='video_id'),
+            on='video_id',
+            how='left',
+            suffixes=('', '_video')
+        )
+
+        feature_columns = [col for col in train_connections.columns if col[:6] == 'onehot']
+        print(feature_columns)
+        X_train = train_connections[feature_columns].values
+        y_train = train_connections['watch_ratio'].values
+        print(f"Training at t={self.t-1} on {X_train.shape[0]} samples with {X_train.shape[1]} features.")
+        print(X_train)
+        print(y_train)
+        self.reg_model.partial_fit(X_train, y_train)
+
+    def predict(self, pred_connections):
+
+        #todo is it reasonable to predict for all possible connections at t?
+
+        if pred_connections.empty:
+            return np.array([]), np.array([])
+        pred_connections = pred_connections.merge(
+            self.user_features,
+            on='user_id',
+            how='left',
+            suffixes=('', '_user')
+        )
+
+        pred_connections = pred_connections.merge(
+            self.video_features.reset_index(drop=False, names='video_id'),
+            on='video_id',
+            how='left',
+            suffixes=('', '_video')
+        )
+        feature_columns = [col for col in pred_connections if col[:6] == 'onehot']
+        X_pred = self.possible[feature_columns].values
+        print(f"Predicting at t={self.t} on {self.possible.shape[0]} samples with {len(feature_columns)} features.")
+        print(X_pred)
+
+        predictions = self.reg_model.predict(X_pred)
+        true = self.possible['watch_ratio'].values
+
+        return predictions, true
+
+    def eval(self, predictions, true):
+        # todo more advanced metric that takes the not connected into account?
+        # simple RMSE
+
+        mask = ~np.isnan(true)
+        mse = np.mean((predictions[mask] - true[mask]) ** 2)
+        rmse = np.sqrt(mse)
+        print(f"RMSE: {rmse}")
+
+        return rmse
+
+    def train(self):
+
+        # no predict for t = 0
+        # todo this is messy, it would be nice to clean the empty time steps
+        connections = self.bi_network.connections_at_t(self.t)
+        # todo 10 for testing to make sure it works, change later to max(timestamp)
+        for i in range(10):
+            self.step(connections)
+            self.train_step(connections)
+            # t incrementd in step
+            connections = self.bi_network.connections_at_t(self.t)
+            predictions, true = self.predict(connections)
+            if predictions.size == 0:
+                continue
+            self.eval(predictions, true)
 
 
 if __name__ == "__main__":
