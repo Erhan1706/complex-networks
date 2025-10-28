b7afd5ba943826c2d5f837d4a54bf721fd9284e2
NewLaptop <piszczekwiktor@gmail.com>
2025-10-28 19:36:00 +0100

    cleaned init of binetwork and fix filter na

--- regression/bi_network.py
+++ regression/bi_network.py
@@ -4,15 +4,15 @@ import os
 
 class BiNetwork:
 
-    def __init__(self, interaction_df, user_features, filter_na_users=True):
+    def __init__(self, interaction_df, user_features, filter_na_users=False):
         # drop all besides one hot encoded features
-        self.user_features = user_features.drop(user_features.columns[list(range(1, 13))], axis=1)
+        user_features = user_features.drop(user_features.columns[list(range(1, 13))], axis=1)
         # self.user_features = user_features.drop(user_features.columns[list([1,2,5,6,8,9,11])], axis=1)
-        self.interaction_df = interaction_df
         if filter_na_users:
             users_with_na = user_features[user_features.isna().any(axis=1)]['user_id']
-            self.user_features = self.user_features[~self.user_features['user_id'].isin(users_with_na)].reset_index(drop=True)
-            self.interaction_df = interaction_df[~interaction_df['user_id'].isin(users_with_na)].reset_index(drop=True)
+            user_features = user_features[~user_features['user_id'].isin(users_with_na)].reset_index(drop=True)
+            interaction_df = interaction_df[~interaction_df['user_id'].isin(users_with_na)].reset_index(drop=True)
+
         interaction_df['timestamp'] = (interaction_df['timestamp'] / 100).round(0)
         interaction_df['timestamp'] = interaction_df['timestamp'] - interaction_df['timestamp'].min()
         timestamps = interaction_df['timestamp'].unique()
@@ -20,14 +20,16 @@ class BiNetwork:
         series = range(len(timestamps))
         timestamp_mapping = dict(zip(timestamps, series))
         interaction_df['timestamp'] = interaction_df['timestamp'].map(timestamp_mapping)
-        self.all_connections = self.interaction_df.pivot_table(index=['user_id', 'video_id'], values=['watch_ratio', 'timestamp'])
         # create full network of all possible user-video connections
         users = interaction_df['user_id'].unique()
         videos = interaction_df['video_id'].unique()
         full_network = pd.MultiIndex.from_product([users, videos], names=['user_id', 'video_id']).to_frame(index=False)
-        full_network = full_network.merge(self.interaction_df[['user_id', 'video_id', 'timestamp', 'watch_ratio']],
+        full_network = full_network.merge(interaction_df[['user_id', 'video_id', 'timestamp', 'watch_ratio']],
                                           on=['user_id', 'video_id'], how='left')
+
         self.all_connections = full_network
+        self.user_features = user_features
+        self.ineraction_df = interaction_df
         # set a compound network to the same shape as all but empty
 
     def connections_at_t(self, t):
